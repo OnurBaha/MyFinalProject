70ed103f33ccc9164178b709d068829cdca7fee9
OnurBaha <koconurbaha@gmail.com>
2023-11-22 06:52:47 +0300

    Project files added.

--- FinalProject/ConsoleUI/Program.cs
+++ FinalProject/ConsoleUI/Program.cs
@@ -2,9 +2,26 @@
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-ProductManager productManager = new ProductManager(new EfProductDal());
+ProductTest();
 
-foreach (var product in productManager.GetByUnitPrice(40,100))
+//CategoryTest();
+
+static void ProductTest()
 {
-    Console.WriteLine(product.ProductName);
+    ProductManager productManager = new ProductManager(new EfProductDal());
+
+    foreach (var product in productManager.GetProductDetails())
+    {
+        Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+    }
+}
+
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
 }
