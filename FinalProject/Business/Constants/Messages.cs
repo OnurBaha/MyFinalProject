100f8319624a59f8bf16ef8a3372cd3fc9d9b3b8
OnurBaha <koconurbaha@gmail.com>
2024-01-04 07:58:21 +0300

    fixed

--- FinalProject/Business/Constants/Messages.cs
+++ FinalProject/Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün Eklendi.";
         public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
+        public static string MaintenanceTime = "Sistem Bakım Zamanı";
+        public static string ProductsListed = "Ürünler Listelendi";
     }
 }
